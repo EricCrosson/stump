585a69956e8b36e5c0a9cbbf9cd1a0ca13dcc07c
Eric Crosson <esc@ericcrosson.com>
2016-02-14 16:28:01 -0600

    add long_description from readme

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='stump',
       version='0.1',
       description='Logs program flow with decorators',
+      long_description=readme(),
       url='http://github.com/EricCrosson/stump',
       author='Eric Crosson',
       author_email='eric.s.crosson@utexas.edu',
