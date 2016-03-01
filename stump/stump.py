e01ed80aab219364ef2861710e744984f7ccf623
Eric Crosson <esc@ericcrosson.com>
2016-03-01 16:48:16 -0600

    enhanced api
    closes #14

--- stump/stump.py
+++ stump/stump.py
@@ -126,6 +126,109 @@ def date(f, *args, **kwargs):
 
 
 @parametrized
+def info(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit. Default logging
+    value: info.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Keyword parameters*
+    - log :: integer
+      - Specifies a custom level of logging to pass to the active logger.
+      - Default: INFO
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def debug(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit. Default logging
+    value: debug.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Keyword parameters*
+    - log :: integer
+      - Specifies a custom level of logging to pass to the active logger.
+      - Default: DEBUG
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.DEBUG})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def warning(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit. Default logging
+    value: warning.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Keyword parameters*
+    - log :: integer
+      - Specifies a custom level of logging to pass to the active logger.
+      - Default: WARNING
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.WARNING})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def error(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit. Default logging
+    value: error.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Keyword parameters*
+    - log :: integer
+      - Specifies a custom level of logging to pass to the active logger.
+      - Default: ERROR
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.ERROR})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
 def put(f, *args, **kwargs):
     """Automatically log progress on function entry and exit. Default logging
     value: info.
@@ -147,6 +250,264 @@ def put(f, *args, **kwargs):
         than there are *args.
 
     """
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def debug_ret(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit. Logging
+    value: debug. The function's return value will be included in the logs.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.DEBUG})
+    kwargs.update({'print_return': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def debug_pre(f, *args, **kwargs):
+    """Automatically log progress on function entry. Logging value: debug.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.DEBUG})
+    kwargs.update({'prefix_only': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def debug_post(f, *args, **kwargs):
+    """Automatically log progress on function exit. Logging value: debug.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.DEBUG})
+    kwargs.update({'postfix_only': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def debug_date(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit with date- and
+    time- stamp. Logging value: debug.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.DEBUG})
+    kwargs.update({'print_time': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def warning_ret(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit. Logging
+    value: warning. The function's return value will be included in the logs.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.WARNING})
+    kwargs.update({'print_return': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def warning_pre(f, *args, **kwargs):
+    """Automatically log progress on function entry. Logging value: warning.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.WARNING})
+    kwargs.update({'prefix_only': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def warning_post(f, *args, **kwargs):
+    """Automatically log progress on function exit. Logging value: warning.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.WARNING})
+    kwargs.update({'postfix_only': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def warning_date(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit with date- and
+    time- stamp. Logging value: warning.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.WARNING})
+    kwargs.update({'print_time': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def error_ret(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit. Logging
+    value: error. The function's return value will be included in the logs.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.ERROR})
+    kwargs.update({'print_return': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def error_pre(f, *args, **kwargs):
+    """Automatically log progress on function entry. Logging value: error.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.ERROR})
+    kwargs.update({'prefix_only': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def error_post(f, *args, **kwargs):
+    """Automatically log progress on function exit. Logging value: error.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.ERROR})
+    kwargs.update({'postfix_only': True})
+    return _stump(f, *args, **kwargs)
+
+
+@parametrized
+def error_date(f, *args, **kwargs):
+    """Automatically log progress on function entry and exit with date- and
+    time- stamp. Logging value: error.
+
+    *Logging with values contained in the parameters of the decorated function*
+    Message (args[0]) may be a string to be formatted with parameters passed to
+    the decorated function. Each '{varname}' will be replaced by the value of
+    the parameter of the same name.
+
+    *Exceptions:*
+    - IndexError and ValueError
+      - will be returned if *args contains a string that does not correspond to
+        a parameter name of the decorated function, or if there are more '{}'s
+        than there are *args.
+
+    """
+    kwargs.update({'log': logging.ERROR})
+    kwargs.update({'print_time': True})
     return _stump(f, *args, **kwargs)
 
 
